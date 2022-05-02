455a2ea35ccc21e3ee1d3d217b50c22ef81c43fe
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2022-05-02 20:59:49 -0300

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,17 +1,20 @@
 # ./youtube_downloader.py
 # Criação: 05/07/2021
-# Versão: 1.2
+# Versão: 1.3
 # Descrição: Script criado com a intenção de baixar videos do Youtube
 # Modo de uso: python youtube_downloader.py <url_do_video_do_YouTube>
 
 from pytube import YouTube
 import sys
 
-# Aqui se define por argumento o link do vídeo do Youtube que será baixado
-video = YouTube(sys.argv[1])
+if len(sys.argv) != 2:
+    print("Modo de uso: python youtube_downloader.py <url_do_video_do_YouTube>")
+else:
+    # Aqui se define por argumento o link do vídeo do Youtube que será baixado
+    video = YouTube(sys.argv[1])
 
-# Define a qualidade do vídeo na mais alta qualidade
-stream = video.streams.get_highest_resolution()
+    # Define a qualidade do vídeo na mais alta qualidade
+    stream = video.streams.get_highest_resolution()
 
-# Pasta onde será baixado o conteúdo
-stream.download(output_path='/tmp/')
+    # Pasta onde será baixado o conteúdo
+    stream.download(output_path='/tmp/')
